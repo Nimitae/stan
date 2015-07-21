985a6e8e395d56d0f12bd5b9d03690242b265590
Nimitae <nimitae@hotmail.com>
2015-07-22 01:22:31 +0800

    quiz completed

--- nimitae/js/quiz.js
+++ nimitae/js/quiz.js
@@ -1,13 +1,46 @@
+var totalQuestions = 5;
+var currentScore = 0;
+var currentQuestion = 0;
+var currentQuestionList = [];
+
+
 $(document).ready(function () {
-    var parts = location.pathname.split('/');
-    if (parts[parts.length - 1] == 'quiz.php') {
-        if (document.getElementById('questionType').innerText == 1) {
-            loadMCQQuestion();
-        } else if (document.getElementById('questionType').innerText == 2) {
-            loadFillBlankQuestion();
-        }
-    }
+    /*
+     var parts = location.pathname.split('/');
+     if (parts[parts.length - 1] == 'quiz.php') {
+     if (document.getElementById('questionType').innerText == 1) {
+     loadMCQQuestion();
+     } else if (document.getElementById('questionType').innerText == 2) {
+     loadFillBlankQuestion();
+     }
+     }
+     */
 });
+
+function startQuiz() {
+    hideStartQuiz();
+    initialiseQuestionArea();
+    currentScore = 0;
+    currentQuestion = 0;
+    var questionType = document.getElementById('questionType').innerText;
+    console.log(questionType);
+    if (questionType == 1) {
+        console.log('here');
+        $("#question-area").prepend('<form method="post" onsubmit="checkAnswer2();return false;"><div id="question-mcq"><h1 class="page-header quiz-header2" draggable="false"><img class="resize" id="progress-bar" src=""></h1><p id="question"></p><br><div class="question-option" id="option1"><input type="radio"></div><div class="question-option" id="option2"><input type="radio"></div><div class="question-option" id="option3"><input type="radio"></div><div class="question-option" id="option4"><input type="radio"></div></div><br><br><input type="submit" class="btn btn-default btn-primary start2" value="Check Answer"></form>');
+        loadMCQQuestion();
+    }
+}
+
+function initialiseQuestionArea() {
+    $('#question-area').empty();
+}
+
+function hideStartQuiz() {
+    $("#quiz-header").hide();
+    $("#quiz-subheader").hide();
+    $("#normal-start").hide();
+    $("#quiz-start").hide();
+}
 
 function loadMCQQuestion() {
     initialiseQuestionFields();
@@ -82,38 +115,53 @@ function checkAnswer(selectedOption) {
         localStorage.setItem('prevQuestionResult', false);
         alert('Nope! Loading new question.');
     }
-    //   } else {
-    //      console.log("Attempting to answer question twice");
-    //  }
     loadMCQQuestion();
 
 }
 
 function checkAnswer2() {
     var answer = $('form').serialize().split("=")[1];
-    if (answer == "option1") {
+    if (answer == currentQuestionList[currentQuestion-1].correct) {
         localStorage.setItem('prevQuestionResult', true);
-        alert('You got that right! Loading new question.');
+        currentScore += 1;
     } else {
         localStorage.setItem('prevQuestionResult', false);
-        alert('Nope! Loading new question.');
     }
-    console.log(answer);
-    loadMCQQuestion();
+    currentQuestionList[currentQuestion - 1].answered = answer;
+
+    if (currentQuestion == totalQuestions) {
+        loadEndQuiz();
+    } else {
+        loadMCQQuestion();
+    }
 }
 
 function displayMCQ(data) {
     var dataObj = jQuery.parseJSON(data);
     console.log(dataObj);
+    currentQuestion += 1;
     localStorage.setItem('correctAnswer', dataObj.option1);
+
     document.getElementById('question').innerText = dataObj.question;
     var ansArray = ['option1', 'option2', 'option3', 'option4'];
     shuffle(ansArray);
-    document.getElementById(ansArray[0]).innerHTML = "<input name='answer' type='radio' value='option1' id='" + ansArray[0] + "Radio'> <label for='" + ansArray[0] + "Radio'>" + dataObj.option1 + "</label>";
-    document.getElementById(ansArray[1]).innerHTML = "<input name='answer' type='radio' value='option2'  id='" + ansArray[1] + "Radio'> <label for='" + ansArray[1] + "Radio'>" + dataObj.option2 + "</label>";
-    document.getElementById(ansArray[2]).innerHTML = "<input name='answer' type='radio' value='option3'  id='" + ansArray[2] + "Radio'> <label for='" + ansArray[2] + "Radio'>" + dataObj.option3 + "</label>";
-    document.getElementById(ansArray[3]).innerHTML = "<input name='answer' type='radio' value='option4'  id='" + ansArray[3] + "Radio'> <label for='" + ansArray[3] + "Radio'>" + dataObj.option4 + "</label>";
+    document.getElementById(ansArray[0]).innerHTML = "<input name='answer' type='radio' value='" +ansArray[0] +  "' id='" + ansArray[0] + "Radio'> <label class='option-label' for='" + ansArray[0] + "Radio'>" + dataObj.option1 + "</label>";
+    document.getElementById(ansArray[1]).innerHTML = "<input name='answer' type='radio' value='" +ansArray[1] +  "'  id='" + ansArray[1] + "Radio'> <label class='option-label' for='" + ansArray[1] + "Radio'>" + dataObj.option2 + "</label>";
+    document.getElementById(ansArray[2]).innerHTML = "<input name='answer' type='radio' value='" +ansArray[2] +  "'  id='" + ansArray[2] + "Radio'> <label class='option-label' for='" + ansArray[2] + "Radio'>" + dataObj.option3 + "</label>";
+    document.getElementById(ansArray[3]).innerHTML = "<input name='answer' type='radio' value='" +ansArray[3] +  "'  id='" + ansArray[3] + "Radio'> <label class='option-label' for='" + ansArray[3] + "Radio'>" + dataObj.option4 + "</label>";
     document.getElementById('questionID').innerText = dataObj.questionID;
+
+    var question = {
+        "question": dataObj.question,
+        "option1": document.getElementById('option1').innerText,
+        "option2": document.getElementById('option2').innerText,
+        "option3": document.getElementById('option3').innerText,
+        "option4": document.getElementById('option4').innerText,
+        "correct": ansArray[0]
+    };
+    currentQuestionList.push(question);
+    console.log(currentQuestionList);
+    updateProgressBar();
 }
 
 function initialiseQuestionFields() {
@@ -136,4 +184,49 @@ function whoops() {
 function shuffle(o) {
     for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
+}
+
+function updateProgressBar() {
+    var progressBarImages = [
+        '',
+        '',
+        '',
+        '',
+        '//i61.tinypic.com/2s84d2h.png'
+    ];
+    console.log(currentQuestion);
+    if (currentQuestion == totalQuestions) {
+        document.getElementById("progress-bar").src = progressBarImages[4];
+    } else if (currentQuestion > totalQuestions * 3 / 4) {
+        document.getElementById("progress-bar").src = progressBarImages[3];
+    } else if (currentQuestion > totalQuestions * 3 / 4) {
+        document.getElementById("progress-bar").src = progressBarImages[2];
+    } else if (currentQuestion > totalQuestions * 3 / 4) {
+        document.getElementById("progress-bar").src = progressBarImages[1];
+    } else if (currentQuestion > totalQuestions * 3 / 4) {
+        document.getElementById("progress-bar").src = progressBarImages[0];
+    }
+}
+
+function loadEndQuiz() {
+    initialiseQuestionArea();
+    $('#question-area').prepend('<div><h1 class="page-header quiz-header" draggable="false">YOUR SCORE: ' + currentScore + '/' + totalQuestions + '</h1></div>' +
+        '<p id="results-area"></p>' +
+        '<a class="btn btn-default btn-primary start3" onclick="startQuiz()">Retake Quiz!</a>');
+
+    for (var i = 0; i < currentQuestionList.length; i++) {
+        $('#results-area').append('<p>' + currentQuestionList[i].question + '</p><br>' +
+            '<div class="result-option" id="' + i + '_' + 1 + '">' + currentQuestionList[i].option1 + '</div>' +
+            '<div class="result-option" id="' + i + '_' + 2 + '">' + currentQuestionList[i].option2 + '</div>' +
+            '<div class="result-option" id="' + i + '_' + 3 + '">' + currentQuestionList[i].option3 + '</div>' +
+            '<div class="result-option" id="' + i + '_' + 4 + '">' + currentQuestionList[i].option4 + '</div>'
+        );
+
+        var correct = currentQuestionList[i].correct.split('option')[1];
+        $('#' + i + '_' + correct).addClass('green-answer');
+        if (currentQuestionList[i].correct != currentQuestionList[i].answered) {
+            var wrong = currentQuestionList[i].answered.split('option')[1];
+            $('#' + i + '_' + wrong).addClass('red-answer');
+        }
+    }
 }
--- nimitae/quiz.php
+++ nimitae/quiz.php
@@ -9,34 +9,21 @@ $category = $moduleServices->getModuleIDOfCategory($_POST['categoryID']);
 include('header.partial.php');
 ?>
 
-            <h2 class="sub-header"><?php print $category->moduleID . ' : ' . $category->title; ?></h2>
-            <?php if ($category->questionType == MCQ) : ?>
-                <p id="question"></p>
-                <br>
+    <h1 class="page-header quiz-header" id="quiz-header" draggable="false">
+        <?php print $category->moduleID . ':' ?>
+    </h1>
+    <h1 class="page-header quiz-subheader" id="quiz-subheader" draggable="false">
+        <?php print $category->title; ?>
+    </h1>
+    <a class="btn btn-default btn-primary quiz" id="quiz-start" href="#">Quiz On/Off</a>
+    <a class="btn btn-default btn-primary start" id="normal-start" onclick="startQuiz()">Start!</a>
 
-                <form method="post" onsubmit="checkAnswer2();return false;">
-                    <p class="question-option" id="option1"><input type="radio"></p>
+    <div id="question-area">
 
-                    <p class="question-option" id="option2"><input type="radio"></p>
-
-                    <p class="question-option" id="option3"><input type="radio"></p>
-
-                    <p class="question-option" id="option4"><input type="radio"></p>
-
-                    <input type="submit" value="Check Answer">
-                </form>
-            <?php elseif ($category->questionType == FILLBLANK) : ?>
-                <form method="post" onsubmit="checkFillBlank();return false;">
-                    <p id="fillBlankQuestion">
-
-                    </p>
-                    <input type="submit" value="Check Answer">
-
-                </form>
-            <?php endif; ?>
-            <div hidden id="categoryID"><?php print $category->categoryID; ?></div>
-            <div hidden id="questionID">0</div>
-            <div hidden id="questionType"><?php print $category->questionType; ?></div>
+    </div>
+    <div hidden id="categoryID"><?php print $category->categoryID; ?></div>
+    <div hidden id="questionID">0</div>
+    <div hidden id="questionType"><?php print $category->questionType; ?></div>
 
 
 <?php include('footer.partial.php');
