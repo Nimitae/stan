021550b91eda6ea484a7f52837342267940a40f2
Nimitae <nimitae@hotmail.com>
2015-07-22 17:21:02 +0800

    Collapsing file directory.

--- nimitae/footer.partial.php
+++
@@ -1,8 +0,0 @@
-</div>
-</div>
-</div><!-- Bootstrap core JavaScript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-</body>
-</html>
--- nimitae/forAllPages.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-DEFINE("MCQ", 1);
-DEFINE("FILLBLANK",2);
