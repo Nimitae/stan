8e0b56e5a569f13b45ee007f055c2bfcda1f0ead
Nimitae <nimitae@hotmail.com>
2015-07-24 17:51:30 +0800

    Removed conflict

--- header.partial.php
+++ header.partial.php
@@ -94,8 +94,4 @@
                 </li>
             </ul>
         </div>
-<<<<<<< HEAD
         <div class="col-sm-9 col-sm-offset-3 col-sm-10 col-sm-offset-2 main">
-=======
-        <div class="col-sm-9 col-sm-offset-3 col-sm-10 col-sm-offset-2 main">
->>>>>>> origin/alvi
